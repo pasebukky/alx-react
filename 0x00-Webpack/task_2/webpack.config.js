30e8473cdd0705634b988b8e4c20d2d59870f10c
pasebukky <olubukunolapase@gmail.com>
2024-06-25 10:45:15 +0100

    load asset

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,18 +1,52 @@
-const path = require("path");
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'production',
+//   entry: './js/dashboard_main.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'public')
+//   },
+//   performance: {
+// 		maxAssetSize: 1000000,
+//     maxEntrypointSize: 1000000
+// 	},
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+//         type: 'asset/resource',
+//         use: [
+//           'file-loader',
+// 					{
+// 						loader: 'image-webpack-loader',
+// 						options: {
+// 							bypassOnDebug: true,
+// 							disable: true,
+// 						},
+// 					},
+// 				],
+//       },
+//     ]
+//   }
+// };
+const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
+  entry: './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   performance: {
-		maxAssetSize: 1000000,
-        maxEntrypointSize: 1000000,
-	},
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000
+  },
   module: {
     rules: [
       {
@@ -22,16 +56,9 @@ module.exports = {
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
-        use: [
-          'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
+        generator: {
+          filename: 'assets/holberton-logo.jpg'
+        },
       },
     ]
   }
