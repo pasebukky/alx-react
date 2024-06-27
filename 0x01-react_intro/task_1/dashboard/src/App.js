140682bf27538f3267d948f33cfc19b168a82b93
pasebukky <olubukunolapase@gmail.com>
2024-06-27 16:12:55 +0100

    Embed expressions into website

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import holberton_logo from './holberton_logo.png';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
