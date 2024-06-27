8c4ae8420d60d4275acfc89502c2d597a02f8b23
pasebukky <olubukunolapase@gmail.com>
2024-06-27 16:31:01 +0100

    Create github pages

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import holberton_logo from './holberton_logo.png';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="Access-container">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="button">OK</button>
+        </div>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
