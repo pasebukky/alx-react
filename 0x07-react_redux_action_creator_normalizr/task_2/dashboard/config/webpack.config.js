f82478982a3c0f55953bb281847035e0d5c905c1
pasebukky <olubukunolapase@gmail.com>
2024-07-22 23:06:19 +0100

    Redux tasks

---
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../dist/index.html'), 
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+  devtool: 'inline-source-map',
+};
